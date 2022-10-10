01caaad79be4e20a130a582faec5cfcf6e605122
jonathan-crepeau <jon.crepeau@gmail.com>
2022-10-10 16:29:01 -0700

    Successful GET Request on /api/albums path for albums object data saved in server.js

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
+// NOTE - Installs Express
 const express = require('express');
+// NOTE Invoke Express
 const app = express();
+// NOTE Setup Port To Bind To:
 const port = process.env.PORT || 3000;
 
+// NOTE Middleware that allows CORS (security):
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// NOTE HTTP GET Request for root path:
 app.get('/', (req, res) => {
     res.send('This that pink venom.');
 })
 
+// NOTE HTTP GET Request for /api/albums path:
+app.get('/api/albums', (req, res) => {
+    res.json(albums);
+})
+
+// NOTE Start Server on Port
 app.listen(port, () => console.log(`App listening at ${port}`))
+
+
+// ANCHOR - Data
+
+const albums = [
+    {
+      title: 'Cupid Deluxe',
+      artist: 'Blood Orange'
+    },
+    {
+      title: 'M3LL155X - EP',
+      artist: 'FKA twigs'
+    },
+    {
+      title: 'Fake History',
+      artist: 'letlive.'
+    }
+  ];
