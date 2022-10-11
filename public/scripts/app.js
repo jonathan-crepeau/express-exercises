17ed3d74c18a87a3562d31c7f54bee03c72add6d
jonathan-crepeau <jon.crepeau@gmail.com>
2022-10-10 20:50:25 -0700

    aajax call to collect albums json info and css styling to display on index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,36 @@
 console.log('Fear is the mind killer.');
+
+$(document).ready(function() {
+    $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/api/albums',
+    dataType: 'json',
+    success: (response) => {
+        // printResponse(response);
+        appendAlbums(response);
+    },
+    error: (error) => {
+        printError(error);
+    }
+  });
+})
+
+function appendAlbums(response) {
+    for (let i = 0; i < response.length; i++) {
+        const singleAlbum = $(`<div class="album"><div>`)
+        const albumTitle = $(`<p class="title">${response[i].title}</p>`);
+        const artistName = $(`<p class="artist">${response[i].artist}</p>`);
+        $(singleAlbum).append(albumTitle)
+        $(singleAlbum).append(artistName);
+        $('#albums-container').append(singleAlbum)
+    }
+}
+
+function printResponse(response) {
+    console.log(response);
+    console.log(response.length);
+}
+
+function printError(error) {
+    console.error(error);
+}
--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const express = require('express');
 const app = express();
 // NOTE Setup Port To Bind To:
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+
+// SECTION Middleware:
 
 // NOTE Middleware that allows CORS (security):
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
+app.use(express.static('views'));
+app.use(express.static(__dirname + '/public'));
+
+// SECTION Routes
 // NOTE HTTP GET Request for root path:
 app.get('/', (req, res) => {
-    res.send('This that pink venom.');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 // NOTE HTTP GET Request for /api/albums path:
@@ -26,7 +34,7 @@ app.get('/api/albums', (req, res) => {
 app.listen(port, () => console.log(`App listening at ${port}`))
 
 
-// ANCHOR - Data
+// ANCHOR Data
 
 const albums = [
     {
